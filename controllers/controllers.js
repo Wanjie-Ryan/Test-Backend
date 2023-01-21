1f8f609baf05a8f66f03c1aa73966d85eeab458f
Wanjie Ryan <104978200+Wanjie-Ryan@users.noreply.github.com>
2023-01-21 22:40:39 +0300

    filtering and sorting basics

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -5,7 +5,7 @@ const getPerson =  async (req, res)=>{
 
     try{
 
-        const human = await Person.find({})
+        const human = await Person.find({}).sort('name age').select('name age email')
 
         res.status(200).json({human, number:human.length})
 
@@ -66,10 +66,69 @@ const updatePerson = async (req, res) => {
         res.status(500).json({msg: error})
     }
 
-
-
-
 }
 
 
-module.exports = {postPerson, getPerson, updatePerson}
+   const singlePerson = async(req, res)=>{
+
+        try{
+
+            const {id:personID} = req.params
+
+            const human = await Person.findOne({_id:personID})
+
+            if(!human){
+
+                res.status(404).json({msg:'Person with id ${personID} cannot be found'})
+            }
+
+            res.status(200).json({human})
+
+
+        }
+
+        catch(error){
+
+            res.status(500).json({msg:error})
+
+
+        }
+    }
+
+
+    // const sorthuman = async(req, res)=>{
+
+
+    //     try{
+
+    //         const human = await Person.find({}).sort('name')
+
+    //         res.status(200).json({human})
+
+
+    //     }
+
+    //     catch(error){
+
+
+    //         res.status(500).json({msg:error})
+
+
+    //     }
+
+
+
+
+    // }
+
+
+
+
+
+
+module.exports = {   postPerson,
+                     getPerson, 
+                     updatePerson,
+                     singlePerson,
+                        
+                }
--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const {postPerson, getPerson, updatePerson} = require('../controllers/controllers')
+const {postPerson, getPerson, updatePerson, singlePerson} = require('../controllers/controllers')
 
 
 
 
 
 router.route('/').get(getPerson).post(postPerson)
-router.route('/:id').patch(updatePerson)
+router.route('/:id').patch(updatePerson).get(singlePerson)
+// router.route('/sf').get(sorthuman)
 // router.route('/newperson').post(postPerson)
 
 // router.post('/', postPerson)
